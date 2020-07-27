662d1f66c34feff1f1609075b7cfa61c544d20cf
Nagisa Ashihara <nemuco.nemu@gmail.com>
2020-07-27 15:51:18 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
+
+@app.route('/hello')    
+def greet():
+    return render_template('index.html')
+
+@app.route('/top')
+def top():
+    return "これがトップページや"
+    
+if __name__ == '__main__':
+    app.run(debug=True)
