5c201207bd9e173197323fb5d320324406d88954
Nagisa Ashihara <nemuco.nemu@gmail.com>
2020-07-28 20:41:56 +0900

    テンプレート完了

--- app.py
+++ app.py
@@ -12,6 +12,24 @@ def greet():
 @app.route('/top')
 def top():
     return "これがトップページや"
+
+
+@app.route('/hello/<text>')    
+def namehello(text):
+    return text + "さんこんにちは"    
+
+@app.route('/index')    
+def index():
+    name = "ashihara"
+    age = 19
+    addres = "幸町"
+    return render_template('index.html',user_name = name,user_address = addres,user_age = age)
     
+    
+@app.route('/weather')
+def weather():
+    weather = "ハレのちユキ"
+    return render_template('weather.html',today_weather = weather)    
+
 if __name__ == '__main__':
     app.run(debug=True)
